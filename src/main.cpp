17ab70d7aeae67d0f796cc8157352f30182f9862
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2025-05-06 20:58:48 +0700

    use main callbacks instead of main()

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,38 @@
+#define SDL_MAIN_USE_CALLBACKS
+
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 #include <imgui.h>
 #include <imgui_impl_SDL3.h>
 #include <imgui_impl_sdlrenderer3.h>
-#include <memory>
-
-#include <iostream>
 
 constexpr int WINDOW_MINIMUM_WIDTH = 1280;
 constexpr int WINDOW_MINIMUM_HEIGHT = 720;
 constexpr char WINDOW_TITLE[] = "SDL3 template";
 constexpr auto WINDOW_FLAGS = SDL_WINDOW_RESIZABLE | SDL_WINDOW_HIGH_PIXEL_DENSITY;
 
-int main(int argc, char **argv)
-{
-    SDL_Init(SDL_INIT_AUDIO| SDL_INIT_VIDEO);
+SDL_Renderer *renderer;
+SDL_Window *window;
 
-    auto window = 
-        SDL_CreateWindow(WINDOW_TITLE, WINDOW_MINIMUM_WIDTH, WINDOW_MINIMUM_HEIGHT, WINDOW_FLAGS);
+SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv)
+{
+    SDL_Init(SDL_INIT_VIDEO);
+
+    window = SDL_CreateWindow(WINDOW_TITLE, WINDOW_MINIMUM_WIDTH, WINDOW_MINIMUM_HEIGHT, WINDOW_FLAGS);
+    if (window == nullptr)
+    {
+        SDL_LogError(SDL_LOG_CATEGORY_VIDEO, "Unable to create a new window.");
+        return SDL_APP_FAILURE;
+    }
 
     SDL_SetWindowMinimumSize(window, WINDOW_MINIMUM_WIDTH, WINDOW_MINIMUM_HEIGHT);
 
-    auto renderer = SDL_CreateRenderer(window, "opengl");
+    renderer = SDL_CreateRenderer(window, nullptr);
+    if (renderer == nullptr)
+    {
+        SDL_LogError(SDL_LOG_CATEGORY_RENDER, "Unable to create a new renderer.");
+        return SDL_APP_FAILURE;
+    }
 
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
@@ -29,45 +40,52 @@ int main(int argc, char **argv)
     ImGui_ImplSDL3_InitForSDLRenderer(window, renderer);
     ImGui_ImplSDLRenderer3_Init(renderer);
 
-    bool is_running{true};
-    while (is_running)
+    bool is_running = true;
+
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppIterate(void *appstate)
+{
+    SDL_RenderClear(renderer);
+
+    ImGui_ImplSDL3_NewFrame();
+    ImGui_ImplSDLRenderer3_NewFrame();
+
+    ImGui::NewFrame();
     {
-        SDL_Event event;
-        while (SDL_PollEvent(&event))
+        if (ImGui::Begin("Hello"))
         {
-            ImGui_ImplSDL3_ProcessEvent(&event);
-            if (event.type == SDL_EVENT_QUIT)
+            if (ImGui::Button("Hello World"))
             {
-                is_running = false;
-                break;
+                SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_INFORMATION, "Hello", "Hello World", window);
             }
         }
-
-        SDL_RenderClear(renderer);
-
-        ImGui_ImplSDL3_NewFrame();
-        ImGui_ImplSDLRenderer3_NewFrame();
-
-        ImGui::NewFrame();
-        {
-            if (ImGui::Begin("Hello"))
-            {
-                if (ImGui::Button("Hello World"))
-                {
-                    SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_INFORMATION, "Hello", "Hello World", window);
-                }
-            }
-            ImGui::End();
-        }
-
-        ImGui::EndFrame();
-        ImGui::Render();
-
-        ImGui_ImplSDLRenderer3_RenderDrawData(ImGui::GetDrawData(), renderer);
-        SDL_RenderPresent(renderer);
-        SDL_Delay(1);
+        ImGui::End();
     }
 
+    ImGui::EndFrame();
+    ImGui::Render();
+
+    ImGui_ImplSDLRenderer3_RenderDrawData(ImGui::GetDrawData(), renderer);
+    SDL_RenderPresent(renderer);
+
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
+{
+    ImGui_ImplSDL3_ProcessEvent(event);
+    if (event->type == SDL_EVENT_QUIT)
+    {
+        return SDL_APP_SUCCESS;
+    }
+
+    return SDL_APP_CONTINUE;
+}
+
+void SDL_AppQuit(void *appstate, SDL_AppResult result)
+{
     ImGui_ImplSDLRenderer3_Shutdown();
     ImGui_ImplSDL3_Shutdown();
     ImGui::DestroyContext();
@@ -76,6 +94,4 @@ int main(int argc, char **argv)
     SDL_DestroyWindow(window);
 
     SDL_Quit();
-
-    return EXIT_SUCCESS;
 }
